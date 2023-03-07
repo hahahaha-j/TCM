079eee448e9fe01a523948f50a42da103b57123f
OIRNOIR <70721372+OIRNOIR@users.noreply.github.com>
2023-03-07 13:23:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-function changeAccentColor() {
-  var accentColor = document.getElementById("accent-picker").value;
-  var accentElements = document.querySelectorAll('.accent');
-  for (var i = 0; i < accentElements.length; i++) {
-    accentElements[i].style.color = accentColor;
-  }
-  var buttons = document.querySelectorAll('nav a, main a');
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].style.backgroundColor = accentColor;
-  }
-}
-
-document.getElementById("accent-picker").onchange = changeAccentColor;
-
-// Rainbow Cursor
 import { rainbowCursor } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
 
-new rainbowCursor();
+document.addEventListener('DOMContentLoaded', () => {
+  function changeAccentColor() {
+    var accentColor = document.getElementById("accent-picker").value;
+    var accentElements = document.querySelectorAll('.accent');
+    for (var i = 0; i < accentElements.length; i++) {
+      accentElements[i].style.color = accentColor;
+    }
+    var buttons = document.querySelectorAll('nav a, main a');
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].style.backgroundColor = accentColor;
+    }
+  }
+  
+  document.getElementById("accent-picker").onchange = changeAccentColor;
+  
+  new rainbowCursor();
+});
