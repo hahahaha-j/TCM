769b13a164eb20ca8b81e50b337bcdc2e9c2fe49
OIRNOIR <70721372+OIRNOIR@users.noreply.github.com>
2023-03-07 13:16:09 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeAccentColor() {
+  var accentColor = document.getElementById("accent-picker").value;
+  var accentElements = document.querySelectorAll('.accent');
+  for (var i = 0; i < accentElements.length; i++) {
+    accentElements[i].style.color = accentColor;
+  }
+  var buttons = document.querySelectorAll('nav a, main a');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].style.backgroundColor = accentColor;
+  }
+}
+
+// Rainbow Cursor
+import { rainbowCursor } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
+
+new rainbowCursor();
